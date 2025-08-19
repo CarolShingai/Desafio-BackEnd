a9d715293c2f034e6f97c93f3cdfcb32d774517b
carolShingai <carolineshingai@gamil.com>
2025-08-18 21:18:29 -0300

    raw: adding default data for DeliveryPerson

--- RentalApi/Infrastructure/Data/RentalDbContext.cs
+++ RentalApi/Infrastructure/Data/RentalDbContext.cs
@@ -62,6 +62,19 @@ namespace RentalApi.Infrastructure.Data
             modelBuilder.Entity<Moto>().HasData(
                 new Moto { Id = 1, Identificador = "moto123", Ano = 2020, Modelo = "Mottu Sport", Placa = "CDX-0101", IsRented = true }
             );
+            modelBuilder.Entity<DeliveryPerson>().HasData(
+                new DeliveryPerson
+                {
+                    Id = Guid.NewGuid(),
+                    Identifier = "entregador123",
+                    Name = "João Silva",
+                    Cnpj = "12345678901234",
+                    BirthDate = "1990-01-01",
+                    Cnh = "12345678901",
+                    CnhType = "A",
+                    CnhImage = "base64string"
+                }
+            );
         }
     }
 }
